29694c8714da6975d5fd825dedfade43f4902d42
Nati Namvong <nati@linecorp.com>
2024-02-12 00:42:22 +0700

    done game state

--- app.vue
+++ app.vue
@@ -1,6 +1,56 @@
 <script setup lang="ts">
-const { players, addPlayer, removePlayer, currentTeamA, currentTeamB, addPlayerToTeam, removePlayerFromTeam, getAvailablePlayers } = usePlayersStore()
+const { currentGame, newGame, endGame, resetAllGames, activitys, addActivity, removeActivity, addPlayer, removePlayer, currentTeamA, currentTeamB, addPlayerToTeam, removePlayerFromTeam, getAvailablePlayers } = useGamesStore()
 const newPlayerName = ''
+
+const currentScore = computed(() => {
+  return {
+    teamA: activitys
+      .filter(
+        (a) =>
+          a.gameId === currentGame.gameId &&
+          a.team === "A" &&
+          a.action === "score"
+      )
+      .reduce((accumulator, object) => {
+        return accumulator + object.score;
+      }, 0),
+    teamB: activitys
+      .filter(
+        (a) =>
+          a.gameId === currentGame.gameId &&
+          a.team === "B" &&
+          a.action === "score"
+      )
+      .reduce((accumulator, object) => {
+        return accumulator + object.score;
+      }, 0)
+  }
+})
+
+const currentPlayerScore = (playerId: number) => {
+  return activitys
+    .filter(
+      (a) =>
+        a.gameId === currentGame.gameId &&
+        a.player.id === playerId &&
+        a.action === 'score'
+    )
+    .reduce((accumulator, object) => {
+      return accumulator + object.score;
+    }, 0)
+}
+
+const currentPlayerAsset = (playerId: number) => {
+  return activitys
+    .filter(
+      (a) =>
+        a.gameId === currentGame.gameId &&
+        a.player.id === playerId &&
+        a.action === 'assit'
+    ).length
+}
+
+
 </script>
 
 <template>
@@ -19,15 +69,82 @@ const newPlayerName = ''
     <h1>Team A</h1>
     <ul>
       <li v-for="item in currentTeamA" :key="item.id">
-        {{ item.id }} - {{ item.name }}
-        <button @click="removePlayerFromTeam('A', item.id)">[Remove Data]</button>
+        {{ item.name }} score({{ currentPlayerScore(item.id) }}) asset({{ currentPlayerAsset(item.id) }})
+        <button @click="removePlayerFromTeam('A', item.id)">[Remove]</button>&nbsp;
+        <!--  -->
+        <button @click="addActivity({
+          id: Date.now(),
+          gameId: currentGame.gameId,
+          team: 'A',
+          player: item,
+          action: 'score',
+          score: 2
+        })">[2 Point]</button>&nbsp;
+        <!--  -->
+        <button @click="addActivity({
+          id: Date.now(),
+          gameId: currentGame.gameId,
+          team: 'A',
+          player: item,
+          action: 'score',
+          score: 3
+        })">[3 Point]</button>&nbsp;
+        <!--  -->
+        <button @click="addActivity({
+          id: Date.now(),
+          gameId: currentGame.gameId,
+          team: 'A',
+          player: item,
+          action: 'assit',
+          score: 0
+        })">[Assit]</button>
       </li>
     </ul>
     <h1>Team B</h1>
     <ul>
       <li v-for="item in currentTeamB" :key="item.id">
-        {{ item.id }} - {{ item.name }}
-        <button @click="removePlayerFromTeam('B', item.id)">[Remove Data]</button>
+        {{ item.name }} score({{ currentPlayerScore(item.id) }}) asset({{ currentPlayerAsset(item.id) }})
+        <button @click="removePlayerFromTeam('B', item.id)">[Remove]</button>&nbsp;
+        <!--  -->
+        <button @click="addActivity({
+          id: Date.now(),
+          gameId: currentGame.gameId,
+          team: 'B',
+          player: item,
+          action: 'score',
+          score: 2
+        })">[2 Point]</button>&nbsp;
+        <!--  -->
+        <button @click="addActivity({
+          id: Date.now(),
+          gameId: currentGame.gameId,
+          team: 'B',
+          player: item,
+          action: 'score',
+          score: 3
+        })">[3 Point]</button>&nbsp;
+        <!--  -->
+        <button @click="addActivity({
+          id: Date.now(),
+          gameId: currentGame.gameId,
+          team: 'B',
+          player: item,
+          action: 'assit',
+          score: 0
+        })">[Assit]</button>
+      </li>
+    </ul>
+    <!--  -->
+    <button @click="newGame()">[New Game]</button>&nbsp;
+    <button @click="endGame()">[End Game]</button>&nbsp;
+    <button @click="resetAllGames()">[Reset All Games]</button>
+    <hr>
+    <h1>Game {{ currentGame.gameId }}</h1>
+    <h2>{{ currentScore }}</h2>
+    <ul>
+      <li v-for="item in activitys" :key="item.id">
+        {{ item.id }} - {{ item.gameId }} - {{ item.team }} - {{ item.player }} - {{ item.action }} - {{ item.score }}
+        <button @click="removeActivity(item.id)">[Remove]</button>
       </li>
     </ul>
   </div>
