42f983f8ef9156808c32a63c170201a1c6ec0550
Nati Namvong <nati@linecorp.com>
2024-02-14 00:34:07 +0700

    viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        },
+      ],
+    },
+  },
   modules: [
     "@pinia/nuxt",
     "@pinia-plugin-persistedstate/nuxt",
