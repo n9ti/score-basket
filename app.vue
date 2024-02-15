8b412dce607a97175e506c3fcbf60f40aba5380c
Nati Namvong <nati@linecorp.com>
2024-02-15 11:20:45 +0700

    pwa

--- app.vue
+++ app.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
 // --------------
 // Disable Pinch Zoom on Mobile Web (iOS)
 // https://stackoverflow.com/questions/11689353/disable-pinch-zoom-on-mobile-web/52201924#52201924
@@ -18,9 +18,27 @@ document.addEventListener('gestureend', function (e) {
   // special hack to prevent zoom-to-tabs gesture in safari
   document.body.style.zoom = 0.99;
 });
+
+// 
+
+const { $pwa } = useNuxtApp()
+onMounted(() => {
+  if ($pwa.offlineReady)
+    alert('App ready to work offline')
+})
 </script>
 
 <template>
+  <NuxtPwaManifest />
+  <div class="p-4" v-show="$pwa.needRefresh">
+    <span>
+      New version available, click on reload button to update.
+    </span>
+
+    <button class="btn btn-accent btn-xs" @click="$pwa.updateServiceWorker()">
+      Reload
+    </button>
+  </div>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
